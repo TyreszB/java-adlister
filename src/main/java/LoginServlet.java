f371093439841691ebed227bdc61af7bc2c40697
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-06-16 13:22:33 -0400

    finished exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,23 +3,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        String isLoggedIn = (String) session.getAttribute("isLoggedIn");
+        if (isLoggedIn != null && isLoggedIn.equals("yes")) {
+            response.sendRedirect("/profile");
+        } else {
+            request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
-
+        HttpSession session = request.getSession();
         if (validAttempt) {
+            session.setAttribute("isLoggedIn", "yes");
+            session.setAttribute("username", username);
             response.sendRedirect("/profile");
         } else {
+            session.removeAttribute("isLoggedIn");
             response.sendRedirect("/login");
         }
+
     }
 }
